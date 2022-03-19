9748aa5f2334e5baf7264321aedf18b961775d2d
JaureguiSergio <2019030342@upsin.edu.mx>
2022-03-18 20:36:45 -0700

    Se agrega el Proyecto

---
+++ App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React, { useEffect, useState } from "react"
+import AsyncAwait from './getApi';
+
+
+
+function App() {
+
+  
+
+  return(
+    <>
+    <h2>Tabla</h2>
+    <AsyncAwait/>
+    </>
+
+  )
+}
+
+export default App;
